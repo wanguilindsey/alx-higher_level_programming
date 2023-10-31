67db30f137d387f4852a7321c666aad679711897
wanguilindsey <wanguilindsey@gmail.com>
2023-10-31 08:08:30 -0700

    update

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for a in range(10):
     for b in range(a + 1, 10):
         if a * 10 + b < 89:
-            print("{:d}{:d}, ".format(a,b), end='')
+            print("{:d}{:d}, ".format(a, b), end='')
         else:
-            print("{:d}{:d}".format(a,b))
+            print("{:d}{:d}".format(a, b))
