9f98be9a4f033c716b6832ba8f4a746f44544192
wanguilindsey <wanguilindsey@gmail.com>
2023-11-04 21:58:30 -0700

    list

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     new_string = ""
-    for element in my_string:
-        if element != "c" and element != "C":
-            new_string += element
-        return new_string
+    for elements in my_string:
+        if elements != "c" and elements != "C":
+            new_string += elements
+    return new_string
