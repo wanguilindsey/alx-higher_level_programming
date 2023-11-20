290f090627f4adf82539e508a0e9a7110ffe0bc1
wanguilindsey <wanguilindsey@gmail.com>
2023-11-20 10:30:30 -0800

    exceptions

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
                 print("{:d}".format(my_list[i]), end="")
                 count += 1
     except IndexError:
-        pass
+        print("\nIndexError: list index out of range")
 
     print()  # New line after printing integers
     return count
