10803ac03bf486ba3d94469a7ea97cbe4436b737
wanguilindsey <wanguilindsey@gmail.com>
2023-11-28 18:53:35 -0800

    more classes

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -40,6 +40,8 @@ class Rectangle:
             return 2 * (self.__width + self.__height)
 
     def __str__(self):
+        rectangle = []
+
         if self.__width == 0 or self.__height == 0:
             return ""
 
