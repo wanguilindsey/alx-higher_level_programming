f799fdaa99e21896437f56eb4550e375da58be7f
wanguilindsey <wanguilindsey@gmail.com>
2024-02-16 03:53:27 -0800

    mysql mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,41 +1,15 @@
 #!/usr/bin/python3
+"""script that lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
-import sys
-
-
-def main():
-    # Check if all three arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: {} username password database_name".format(sys.argv[0]))
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    # Connect to MySQL server
-    db = MySQLdb.connect(host="localhost",
-                         user=username,
-                         passwd=password,
-                         db=database_name,
-                         port=3306)
-
-    # Create a cursor object using cursor() method
-    cursor = db.cursor()
-
-    # Execute SQL query to select all states
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-    # Fetch all the rows using fetchall() method
-    results = cursor.fetchall()
-
-    # Display the results
-    for row in results:
-        print(row)
-
-    # Disconnect from server
-    db.close()
-
+from sys import argv
 
 if __name__ == "__main__":
-    main()
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
