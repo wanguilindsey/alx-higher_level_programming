fb66efe373c2775681c01bd3715f01c693f1985f
wanguilindsey <wanguilindsey@gmail.com>
2024-03-26 04:56:16 -0700

    js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,10 +5,6 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const content = process.argv[3];
 
-fs.writeFile(filePath, content, 'utf-8', (err) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log(`Content successfully written to ${filePath}`);
+fs.writeFile(filePath, content, err => {
+  if (err) console.log(err);
 });
